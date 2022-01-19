6d5b3bf97c6a0a74b6642be36e700ae3f32cdf79
Christian Marienfeld <post@zwiebelgasse.de>
2022-01-20 00:18:27 +0100

    version 6 - exel export

--- lerntutoren/admin/custom.php
+++ lerntutoren/admin/custom.php
@@ -14,9 +14,8 @@ class extLerntutorenAdminCustom extends AbstractPage {
 
 	public function execute() {
 
-		//$this->getRequest();
+		//$request = $this->getRequest();
 		//$this->getAcl();
-		
 
 
         $this->render([
@@ -31,5 +30,82 @@ class extLerntutorenAdminCustom extends AbstractPage {
 
 	}
 
+    public function taskExportExel() {
+
+        include_once PATH_EXTENSION . '../models' . DS . 'Slot.class.php';
+        include_once PATH_EXTENSION . '../models' . DS . 'Tutoren.class.php';
+
+        $items = extLerntutorenModelTutoren::getAllByStatus();
+
+        $excelFile = new PHPExcel();
+        $today = date('d_m_Y', time() );
+
+        $excelFile->getProperties()
+            ->setCreator(DB::getGlobalSettings()->siteNamePlain)
+            ->setTitle('Lerntutoren '.$today )
+            ->setLastModifiedBy(DB::getGlobalSettings()->siteNamePlain)
+            ->setDescription('Export der Lerntutoren vom '.$today);
+
+        $excelFile->setActiveSheetIndex(0)->setCellValue("A1", 'ID');
+        $excelFile->getActiveSheet()->getStyle("A1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("B1", 'Status');
+        $excelFile->getActiveSheet()->getStyle("B1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("C1", 'Name');
+        $excelFile->getActiveSheet()->getStyle("C1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("D1", 'Fach');
+        $excelFile->getActiveSheet()->getStyle("D1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("E1", 'Jahrgang');
+        $excelFile->getActiveSheet()->getStyle("E1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("F1", 'Einheiten');
+        $excelFile->getActiveSheet()->getStyle("F1")->getFont()->setBold(true);
+
+        $excelFile->setActiveSheetIndex(0)->setCellValue("H1", 'Slot ID');
+        $excelFile->getActiveSheet()->getStyle("H1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("I1", 'Status');
+        $excelFile->getActiveSheet()->getStyle("I1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("J1", 'Name');
+        $excelFile->getActiveSheet()->getStyle("J1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("K1", 'Einheiten');
+        $excelFile->getActiveSheet()->getStyle("K1")->getFont()->setBold(true);
+        $excelFile->setActiveSheetIndex(0)->setCellValue("M1", 'Info');
+        $excelFile->getActiveSheet()->getStyle("M1")->getFont()->setBold(true);
+
+        $i = 2;
+        foreach ($items as $item) {
+
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(0,$i, $item->getID());
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(1,$i, $item->getStatus());
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(2,$i, $item->getTutor()->getCollection()['name'].' ('.$item->getTutor()->getCollection()['klasse'].')');
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(3,$i, $item->getFach());
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(4,$i, $item->getJahrgang());
+            $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(5,$i, $item->getEinheiten());
+
+            foreach ($item->getSlotsCollection() as $slot) {
+                $i++;
+
+                $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(7,$i, $slot['id']);
+                $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(8,$i, $slot['status']);
+                $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(12,$i, $slot['user']['name']);
+                $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(9,$i, $slot['einheiten']);
+                $excelFile->setActiveSheetIndex(0)->setCellValueByColumnAndRow(10,$i, $slot['info']);
+            }
+            $i++;
+        }
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="Lerntutoren_'.$today.'.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+        // If you're serving to IE over SSL, then the following may be needed
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+        $objWriter = PHPExcel_IOFactory::createWriter($excelFile, 'Excel2007');
+        $objWriter->save('php://output');
+        exit();
+
+    }
 
 }
--- lerntutoren/admin/tmpl/custom.tmpl.php
+++ lerntutoren/admin/tmpl/custom.tmpl.php
@@ -1,5 +1,6 @@
 <div class="box">
 	<div class="box-body">
+        <a class="si-btn" href="<?php echo URL_SELF; ?>&task=exportExel">Export Exel</a>
 		<div id=app></div>
 	</div>
 </div>
